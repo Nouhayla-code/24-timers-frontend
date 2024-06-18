e0457cba9a86ee233da64634de591ab66c31ff33
Nouhayla-code <nouhayla@hotmail.dk>
2024-06-18 11:24:50 +0200

    .

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+//import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -17,6 +22,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   );
 }
